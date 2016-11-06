2568ab36d93990f25ba31b2e45c37b0324928588
Vilhjalmur Alex <vilhjalmuralex@gmail.com>
2016-11-06 17:26:48 +0000

    final final

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -8,7 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
-		config.title = "Mesh and Texture exercise"; // or whatever you like
+		config.title = "Space Infiltrators"; // or whatever you like
 		config.width = 1024;  //experiment with
 		config.height = 512;  //the window size
 		config.x = 80;
